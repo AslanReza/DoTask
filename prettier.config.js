d74cfd85527d9a1d5e15a72dd4aaee3a916a52fe
Aslan <aslanreza77@gmail.com>
2024-12-14 19:46:03 +0330

    Add configuration files: MAKEFILE, Prettier, Renovate, SECURITY, CODE_OF_CONDUCT, AUTHORS, CONTRIBUTING, LICENSE

--- prettier.config.js
+++ prettier.config.js
@@ -1,8 +1,15 @@
 module.exports = {
-    semi: true,
-    singleQuote: true,
-    trailingComma: 'es5',
-    tabWidth: 2,
-    printWidth: 80
-  };
-  
+  singleQuote: true,
+  trailingComma: 'es5',
+  printWidth: 80,
+  tabWidth: 2,
+  semi: false,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  arrowParens: 'always',
+  requirePragma: false,
+  insertPragma: false,
+
+  endOfLine: 'lf',
+  jsonRecursiveFormat: true,
+};
