02327422c3b78037db4337c13c58d93105a0a113
Aslan <aslanreza77@gmail.com>
2024-12-15 18:51:11 +0330

    Set up TailwindCSS with Custom Theme for glassy and Neumorphic Styling

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        glassy: 'rgba(255, 255, 255, 0.2)',
+      },
+      boxShadow: {
+        neu: '8px 8px 16px #b8c6db, -8px -8px 16px #ffffff',
+      },
+    },
+  },
+  plugins: [],
+}
