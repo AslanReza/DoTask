08a7af1e697a79a88ec151a4cf0296f97333daa5
Aslan <aslanreza77@gmail.com>
2024-12-15 19:04:34 +0330

    Add React Router and basic Navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="min-h-screen bg-gray-100 text-gray-800">
+        <nav className="p-4 flex justify-between items-center bg-white shadow-md">
+          <div className="font-bold text-xl">On Task</div>
+          <div className="flex gap-4">
+            <Link to={'/'} className="hover:text-blue-500">
+              Tasks
+            </Link>
+            <Link to={'/new'} className="hover:text-blue-500">
+              New Task
+            </Link>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<div>Tasks Page</div>} />
+          <Route path="/new" element={<div>New Task Page</div>} />
+        </Routes>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
