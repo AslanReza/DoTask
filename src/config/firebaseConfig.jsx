16d0fc372bd34b7228c606a72ac119cbf647f8e2
Aslan <aslanreza77@gmail.com>
2024-12-19 00:32:10 +0330

    Refactor API Settings

--- src/config/firebaseConfig.jsx
+++ src/config/firebaseConfig.jsx
@@ -1,22 +1,27 @@
-// Import Firebase
+// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import { getAnalytics } from 'firebase/analytics'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Firebase configuration object
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: 'ontask-7a3d6.firebaseapp.com',
-  projectId: 'ontask-7a3d6',
-  storageBucket: 'ontask-7a3d6.firebasestorage.app',
-  measurementId: 'G-T38WMCHC99',
-  messagingSenderId: '743462603921',
-  appId: '1:743462603921:web:d8610f6f0494fe05b44330',
+  authDomain: 'ontask-616ee.firebaseapp.com',
+  projectId: 'ontask-616ee',
+  storageBucket: 'ontask-616ee.firebasestorage.app',
+  messagingSenderId: '927525621270',
+  appId: '1:927525621270:web:ca0515366f61a06b086f37',
+  measurementId: 'G-TP90ECKZ71',
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
 
-// Export Firebase services
+// Initialize Firebase services
 export const auth = getAuth(app)
 export const db = getFirestore(app)
